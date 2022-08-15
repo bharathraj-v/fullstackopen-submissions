13201c5c75789a8f3ea2564653ceeb8c0a09cfdc
Bharath Raj <bharathrajpalivela@gmail.com>
2022-08-15 13:22:34 +0530

    2.6-2.10

---
+++ part2/countries/src/App.js
@@ -0,0 +1,49 @@
+import { useEffect, useState} from 'react'
+import axios from 'axios'
+
+
+const Filter=({filterVal, onChange}) => {
+  return (
+    <>
+    find countries <input value={filterVal} onChange={onChange}/>
+    </>
+  )
+}
+
+const Display = ({countries, search}) => {
+  const filtered = []
+  countries.map(country => country.name.common.toLowerCase().startsWith(search.toLowerCase()) ? filtered.push(country.name.common):console.log("Filtered"))
+  
+  return filtered.length > 1 ? 
+  filtered.map(country=> <p key={filtered.indexOf(country)}> {country}</p>) 
+  : displayCountryData
+  // return countryDisplay()
+}
+
+
+
+const App = () => {
+  const [search, setSearch] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+    .get('https://restcountries.com/v3.1/all')
+    .then(response => {
+      const countryData = response.data
+      setCountries(countryData)
+    })
+  }, [])
+
+  const handleSearchChange = (event) => setSearch(event.target.value)
+  return (
+  <>
+  <Filter filterVal={search} onChange={handleSearchChange}/>
+  <Display countries={countries} search={search}/>
+  </>
+  )
+
+
+}
+
+export default App
